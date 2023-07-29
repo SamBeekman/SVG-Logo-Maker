bf3b4506173c7f7a5a732938007983ca0a9322f0
Slug <sambeekman@hotmail.com>
2023-07-29 14:53:11 +1000

    added comments

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// code to initiate the application, use either 'node index.js' or 'npm start' from the terminal window or 'npm run test' to run the tests.
 const CLI = require('./lib/cli');
 
 const newCLI = new CLI();
